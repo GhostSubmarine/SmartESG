ea7e42c3168a10beb52c63f4ebb9c21c793f244e
GhostSubmarine <35192444+GhostSubmarine@users.noreply.github.com>
2024-05-20 07:29:02 +0800

    提交代码

--- src/pages/main/index.vue
+++ src/pages/main/index.vue
@@ -21,6 +21,74 @@
         </div>
       </article>
     </section>
+    <section>
+      <article>
+        <div class="container">
+          <div class="content">
+            <h3 style="padding-top: 0;">
+              领先的ESG综合解决⽅案供应商。
+            </h3>
+            <p>⽴⾜中国市场为全球应对⽓候变化与低碳转型提供重要的研究，⼯具与数据以助⼒合作伙伴做出更明智的选择，从⽽积极应对⽓候变化等系统性⻛险，共创绿⾊永续未来！</p>
+          </div>
+          <div class="media">
+            <div class="media-card">
+              <img src="../../assets/images/微信图片_20240414101955.png" />
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section>
+      <article>
+        <div class="container">
+          <div class="content">
+            <h3 style="padding-top: 0;">
+              Smartesg汇聚全球强⼤的ESG投研量化研究团队，聚焦本⼟研究和客⼾服务，以中国智慧服务全球。
+            </h3>
+            <p>六⼤核⼼优势： 全球视野, 专业定制, 产业专家, 以利并举, 多元合作, 数智赋能</p>
+          </div>
+          <div class="media">
+            <div class="media-card">
+              <img src="../../assets/images/微信图片_20240414105125.png" />
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section>
+      <article>
+        <div class="container">
+          <div class="content" style="margin-left: 0; margin-right: auto;">
+            <h3 style="padding-top: 0;">
+              主要客⼾群体
+            </h3>
+            <!-- <p>通过数字化变⾰传统分析，提供有效ESG解决⽅案。赋能绿⾊⾦融，推动可持续发展。⽴⾜中国市场，服务全球低碳转型需求。提供关键数据，助企业与机构做出明智决策。积极应对⽓候变化挑战，促进社会低碳转型。依托国际化专家团队，深度解析⻛险与机会。指导客⼾有效⾏动，共同迈向净零⽬标。从ESG披露⾄碳中和管理，累积丰富经验。</p> -->
+          </div>
+          <div class="media">
+            <div class="media-card">
+              <img src="../../assets/images/微信图片_20240414105141.png" />
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section>
+      <article>
+        <div class="container">
+          <div class="content" style="margin-left: 0; margin-right: auto;">
+            <h3 style="padding-top: 0;">
+              可持续⻅解
+            </h3>
+            <!-- <p>通过数字化变⾰传统分析，提供有效ESG解决⽅案。赋能绿⾊⾦融，推动可持续发展。⽴⾜中国市场，服务全球低碳转型需求。提供关键数据，助企业与机构做出明智决策。积极应对⽓候变化挑战，促进社会低碳转型。依托国际化专家团队，深度解析⻛险与机会。指导客⼾有效⾏动，共同迈向净零⽬标。从ESG披露⾄碳中和管理，累积丰富经验。</p> -->
+          </div>
+          <div class="media">
+            <div class="media-card">
+              <img src="../../assets/images/微信图片_20240414105203.png" />
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
   </main>
 </template>
 
@@ -38,7 +106,15 @@
     padding-top: 1.25rem;
     padding-bottom: 2.5rem;
     display: block;
-
+    .article-container {
+      display: flex;
+      flex-direction: column;
+      .article-content {
+        flex: 1 0 100%;
+        padding-top: 1.5rem;
+        padding-bottom: 3rem;
+      }
+    }
     article {
       width: 100%;
       max-width: calc(1380px - 10.5rem);
@@ -101,7 +177,10 @@
     section {
       padding-top: 2rem;
       padding-bottom: 4rem;
-
+      .article-container {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+      }
       article {
         .container {
           display: grid;
@@ -142,8 +221,8 @@
       flex-direction: column;
       img {
         border-radius: 1rem 1rem 1rem 0;
-        height: 100%;
-        object-fit: cover;
+        // max-height: 100%;
+        // object-fit: cover;
         max-width: 100%;
       }
     }
@@ -159,6 +238,11 @@
       padding-left: 3rem;
       .media-card {
         height: calc(100% - 1.5rem);
+        img {
+          height: 400px;
+          max-height: 400px;
+          margin: auto 0;
+        }
       }
     }
   }
